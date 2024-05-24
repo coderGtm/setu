18413689ee16618726945bcd9404b660bf51b227
coderGtm <gautammehta0000@gmail.com>
2024-05-24 12:56:58 +0530

    added signup and login controllers

---
+++ server/controllers/user.js
@@ -0,0 +1,75 @@
+import User from "../models/User";
+import bcrypt from "bcyptjs";
+
+async function handleUserSignup(req, res) {
+    const { username, email, password } = req.body;
+    
+    try {
+        let user = await User.findOne({ email });
+        if (user) {
+            return res.status(400).json({ msg: "This email is already registered!" });
+        }
+        user = await User.findOne({ username });
+        if (user) {
+            return res.status(400).json({ msg: "This username is already taken!" });
+        }
+
+        user = new User({ username, email, password });
+
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(password, salt);
+
+        await user.save();
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+            if (err) throw err;
+            res.status(200).json({token});
+        })
+    }
+    catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
+
+async function handleUserLogin(req, res) {
+    const { email, password } = req.body;
+
+    try {
+        let user = await User.findOne({ email });
+        if (!user) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(400).json({ msg: 'Invalid credentials' });
+        }
+
+        const payload = {
+            user: {
+                id: user.id
+            }
+        };
+
+        jwt.sign(payload, process.env.JWT_SECRET, (err, token) => {
+            if (err) throw err;
+            res.status(200).json({ token });
+        });
+
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server error');
+    }
+}
+
+module.exports = {
+    handleUserSignup,
+    handleUserLogin
+};
