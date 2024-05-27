4d00dd488a24a6908bee5061ac1277f40dbac5ea
coderGtm <gautammehta0000@gmail.com>
2024-05-27 13:36:11 +0530

    conditional rendering of home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -2,11 +2,15 @@ import React from 'react';
 import UrlForm from '../components/UrlForm';
 
 function Home() {
+    const isLoggedIn = localStorage.getItem("authToken") ? true : false;
+    
+    
     return (
         <div>
             <h1>Setu Home Page</h1>
             <p>Served by Reactjs</p>
-            <UrlForm />
+            {isLoggedIn && <UrlForm />}
+            {!isLoggedIn && <button onClick={() => window.location.href = "/login"}>Login</button>}
         </div>
     )
 }
